0581f857358f050c08b6d1c621dd4d11eadacfd3
MartinCout <martincoutinho00@gmail.com>
2022-07-26 09:34:28 -0300

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
